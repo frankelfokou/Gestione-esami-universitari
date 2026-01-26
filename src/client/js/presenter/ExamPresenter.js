098a736d6fe48d2f7458016bed19ba73b50962ab
arnaski <agu250603@gmail.com>
2026-01-26 11:51:42 +0100

    creazione del ExamPresenter
    Aggiornamento del main con la struttura corretta del MVP

---
+++ src/client/js/presenter/ExamPresenter.js
@@ -0,0 +1,38 @@
+export class ExamPresenter {
+    constructor(view, model) {
+        this.view = view;
+        this.model = model;
+        
+        
+        this.init();
+    }
+    
+    async init() {
+        this.view.bindAddExam(this.handleAddExam.bind(this));
+        
+        
+        await this.refresh();
+    }
+     
+    // Ricarica tabella E statistiche
+    async refresh() {
+        
+        const esami = await this.model.getAll(); 
+        const stats = this.model.getStats(); 
+
+        
+        this.view.renderTable(esami);
+
+        
+        this.view.updateStats(
+            stats.mediaA,
+            stats.mediaP,
+            stats.proiezione
+        );
+    }
+    
+    async handleAddExam(esame) {
+        await this.model.add(esame);
+        await this.refresh();
+    }
+}
